9658fa952da9f1c4e25d905115a4c90d74f63b20
Tathapong Subin <tathapong@gmail.com>
2023-07-19 03:04:16 +0700

    Update errorMiddleware.js
    add console.log for log error

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,3 +1,4 @@
 module.exports = (err, req, res, next) => {
+  console.log(err);
   res.status(err.statusCode || 500).json({ error: err.message || "Something went wrong. Please try again later." });
 };
