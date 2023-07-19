3371127574ed934303686ec52b3ad88ad1c895ce
Tathapong <tathapong.gmail.com>
2023-07-19 20:26:30 +0700

    update app js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -7,16 +8,14 @@ const mysql = require("mysql2/promise");
 app.use(cors());
 
 app.use("/test", async function (req, res, next) {
-  const connection = await mysql.createConnection({
-    host: "aws.connect.psdb.cloud",
-    user: "z91u5hfvp46fkar8vvs9",
-    password: "pscale_pw_tNnXAr72Q0T9ELOoTym0zbxIwKVt0bP1XZrbUyqwQ3F",
-    database: "campboy",
-    ssl: { rejectUnauthorized: true }
-  });
+  try {
+    const connection = await mysql.createConnection(process.env.DATABASE_URL);
 
-  const [rows, fields] = await connection.execute("select * from users");
-  return res.status(200).json({ users: rows });
+    const [rows, fields] = await connection.execute("select * from users");
+    return res.status(200).json({ users: rows });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(8000, () => console.log("server is running on port 8000"));
